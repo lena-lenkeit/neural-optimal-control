0bcb135006e5eb7c5a80340a4767b21f1af9722e
llenkeit <lena.lenkeit1@gmail.com>
2023-04-19 11:49:07 +0200

    Fixed import

--- src/optimal_control/environments/base.py
+++ src/optimal_control/environments/base.py
@@ -4,8 +4,8 @@ import jax.numpy as jnp
 from jax import lax
 from jaxtyping import Array, PyTree
 
+import optimal_control.controls as controls
 import optimal_control.environments as environments
-import optimal_control.solvers as solvers
 
 EnvironmentState = PyTree
 
@@ -15,6 +15,6 @@ class AbstractEnvironment(eqx.Module):
         ...
 
     def integrate(
-        self, control: solvers.AbstractControl, state: environments.EnvironmentState
+        self, control: controls.AbstractControl, state: environments.EnvironmentState
     ) -> Array:
         ...
