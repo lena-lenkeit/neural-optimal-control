64ed90064908ede309e80f0a7fb75132e7a5504b
llenkeit <lena.lenkeit1@gmail.com>
2023-07-06 18:28:25 +0200

    Basic control building skeleton

--- src/optimal_control/solvers/base.py
+++ src/optimal_control/solvers/base.py
@@ -4,7 +4,7 @@ from typing import Callable, List, Tuple
 import equinox as eqx
 import jax
 import jax.numpy as jnp
-from jaxtyping import Array, ArrayLike, PyTree
+from jaxtyping import Array, ArrayLike, PyTree, Scalar
 
 import optimal_control.constraints as constraints
 import optimal_control.controls as controls
@@ -106,13 +106,43 @@ def evaluate_reward(
     reward_fn: Callable[[PyTree], float],
     num_control_points: int,
     key: jax.random.KeyArray,
-) -> float:
+) -> Tuple[Scalar, controls.AbstractControl]:
     # 1. Build the control for the environment
 
+    eval_control = None
+    ret_control = None
     try_project = True
 
-    ## Check if this control implicitly encodes another control
+    # Check if this control implicitly encodes another control
     get_implicit_control_fn = getattr(control, "get_implicit_control", None)
     if exists(get_implicit_control_fn):
         control = get_implicit_control_fn()
+
+        # Stop the implicitly encoded control from being projected
         try_project = False
+
+    # 2. Try to bake constraints into the control
+    # Project -> Transform -> Penalty
+
+    # Try to project the parameters of the control, such that the outputs lie in the
+    # constrained region
+    could_project = False
+    if try_project:
+        apply_projection_fn = getattr(control, "apply_projection", None)
+
+        if exists(apply_projection_fn):
+            for constraint in constraint_chain:
+                apply_projection_fn = getattr(control, "apply_projection")
+                control = apply_projection_fn(constraint.project)
+
+            could_project = True
+
+    # Try to transform the control outputs, such that they lie in the constrained region
+    could_transform = False
+    if not could_project:
+        # In this case, we need to cache the control onto interpolation curves
+        ...
+
+    # Try to create penalty terms for each constraint
+    if not could_transform:
+        ...
