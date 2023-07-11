d91055e246bde804e53dfc44b86dc5210b940993
llenkeit <lena.lenkeit1@gmail.com>
2023-07-11 17:11:01 +0200

    Control submodule imports

---
+++ src/optimal_control/controls/__init__.py
@@ -0,0 +1,7 @@
+from optimal_control.controls.base import (
+    AbstractConstrainableControl,
+    AbstractControl,
+    ControlOutput,
+    MemoryOutput,
+)
+from optimal_control.controls.controls import InterpolationCurveControl, LambdaControl
